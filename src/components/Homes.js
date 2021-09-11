9ed3125f9d1c232a099de962898cab8238f6c6dd
numoonchld <raghavendra.saralaya@gmail.com>
2021-09-11 16:43:56 +0530

    show homes test passes

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Homes() {
+  const [homes, setHomes] = useState([]);
+
+  useEffect(() => {
+    const homesDataPromise = Promise.resolve([
+      {
+        title: "Test Home 1",
+        image: "listing.jpg",
+        location: "Test Location 1",
+        price: 1,
+      },
+      {
+        title: "Test Home 2",
+        image: "listing.jpg",
+        location: "Test Location 2",
+        price: 2,
+      },
+      {
+        title: "Test Home 3",
+        image: "listing.jpg",
+        location: "Test Location 3",
+        price: 3,
+      },
+      {
+        title: "Test Home 4",
+        image: "listing.jpg",
+        location: "Test Location 4",
+        price: 4,
+      },
+    ]);
+
+    homesDataPromise.then((data) => {
+      // console.log(data);
+      setHomes(data);
+    });
+  }, []);
+
   return (
     <div>
-      Homes
+      {homes.map((home) => (
+        <div key={home.title} data-testid="home">
+          {home.title}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Homes
+export default Homes;
--- src/components/Homes.test.js
+++ src/components/Homes.test.js
@@ -1,13 +1,15 @@
 import { render, getAllByTestId } from "@testing-library/react";
 import React from "react";
+import { act } from "react-dom/test-utils";
 import Homes from "./Homes";
 
 let container = null;
-beforeEach(() => {
+beforeEach(async () => {
   container = render(<Homes />).container;
+  await act(async () => {});
 });
 
 it("should show homes", () => {
-  const homes = getAllByTestId("home");
+  const homes = getAllByTestId(container, "home");
   expect(homes.length).toBeGreaterThan(0);
 });
